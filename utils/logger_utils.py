cfd2e2b9033d11f2815abbe300d1002272819ff0
kramerkat <kevzhuart@gmail.com>
2025-01-19 23:23:46 -0500

    Simplify logger_util

--- utils/logger_utils.py
+++ utils/logger_utils.py
@@ -3,43 +3,6 @@ from logging.handlers import TimedRotatingFileHandler
 
 
 class CustomLogger(logging.Logger):
-    """
-    Custom logger class for managing logging configurations with console and file handlers.
-
-    This class provides an easy-to-use interface to configure logging for different
-    components of the bot or application, supporting log file rotation and custom log handlers.
-
-    Attributes
-    ----------
-    name : str
-        The name of the logger instance (used to distinguish different loggers).
-    log_file : str
-        Path to the log file for file logging. Defaults to 'default.log'.
-    max_bytes : int
-        Maximum size of the log file before it rotates (in bytes). Defaults to 32 MiB.
-    backup_count : int
-        Number of backup log files to keep. Defaults to 5.
-    log_level : int
-        The logging level (e.g., logging.DEBUG, logging.INFO). Defaults to logging.INFO.
-    logger : logging.Logger
-        The logging instance.
-
-    Methods
-    -------
-    __init__(self, name, log_to_console=True, log_to_file=True, max_bytes=32 * 1024 * 1024, backup_count=5,
-              log_level=logging.INFO)
-        Initializes the CustomLogger instance with provided parameters.
-    _configure_logger(self)
-        Configures the logger with both console and rotating file handlers.
-    _get_formatter(self)
-        Returns the default formatter for the logger.
-    set_level(self, level)
-        Sets the log level for the logger.
-    add_custom_handler(self, handler)
-        Adds a custom logging handler to the logger.
-    get_logger(self)
-        Returns the configured logger instance.
-    """
 
     def __init__(
         self,
@@ -77,25 +40,6 @@ class CustomLogger(logging.Logger):
         self.setLevel(log_level)  # Set the log level for the logger
         self._configure_logger()  # Configure the logger with handlers
 
-    def _configure_logger(self):
-        """
-        Configures the logger by adding both a console handler and a rotating file handler.
-
-        This method is called automatically during initialization to ensure the logger
-        is configured with appropriate handlers (console and file).
-        """
-        # Clear any existing handlers
-        if self.hasHandlers():
-            self.handlers.clear()
-
-        # Set up the console handler (if enabled)
-        if self.log_to_console:
-            self.addHandler(self.console_handler())
-
-        # Set up the rotating file handler (if enabled)
-        if self.log_to_file:
-            self.addHandler(self.file_handler())
-
     def console_handler(self):
         console_handler = logging.StreamHandler()
         console_handler.setFormatter(self._get_formatter())
@@ -111,51 +55,32 @@ class CustomLogger(logging.Logger):
         file_handler.setFormatter(self._get_formatter())
         return file_handler
 
-    def _get_formatter(self):
-        """
-        Returns the default log message formatter.
+    def set_level(self, level):
+        self.setLevel(level)
 
-        Returns
-        -------
-        logging.Formatter
-            The formatter for the log message.
-        """
+    def add_custom_handler(self, handler):
+        handler.setFormatter(self._get_formatter())
+        self.addHandler(handler)
+
+    def get_logger(self):
+        return self
+
+    def _get_formatter(self):
         return logging.Formatter(
             "[{asctime}] [{levelname:6}] <{name}> {filename}:{lineno} {message}",
             "%Y-%m-%d %H:%M:%S",
             style="{",
         )
 
-    def set_level(self, level):
-        """
-        Sets the log level for the logger.
+    def _configure_logger(self):
+        # Clear any existing handlers
+        if self.hasHandlers():
+            self.handlers.clear()
 
-        Parameters
-        ----------
-        level : int
-            The logging level to set (e.g., logging.DEBUG, logging.INFO).
-        """
-        self.setLevel(level)
+        # Set up the console handler (if enabled)
+        if self.log_to_console:
+            self.addHandler(self.console_handler())
 
-    def add_custom_handler(self, handler):
-        """
-        Adds a custom logging handler to the logger.
-
-        Parameters
-        ----------
-        handler : logging.Handler
-            The custom logging handler to add (e.g., logging.FileHandler, logging.SMTPHandler).
-        """
-        handler.setFormatter(self._get_formatter())
-        self.addHandler(handler)
-
-    def get_logger(self):
-        """
-        Returns the configured logger instance.
-
-        Returns
-        -------
-        logging.Logger
-            The logger instance that has been configured with the appropriate handlers.
-        """
-        return self
+        # Set up the rotating file handler (if enabled)
+        if self.log_to_file:
+            self.addHandler(self.file_handler())
